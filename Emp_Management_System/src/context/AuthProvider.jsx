0003d7b4555d00ab95b2821792d87f9792fb9754
Lokesh Kumar <23981a0549@raghuenggcollege.in>
2026-09-21 18:43:25 +0530

    Update AuthProvider.jsx

--- Emp_Management_System/src/context/AuthProvider.jsx
+++ Emp_Management_System/src/context/AuthProvider.jsx
@@ -1,6 +1,8 @@
 import React, { useState, createContext } from "react";
 import { employee, admin } from "../Utils/LocalStorage";
-
+console.log("AUTH FILE LOADED");
+console.log("EMPLOYEE:", employee);
+console.log("ADMIN:", admin);
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
